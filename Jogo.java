26ae3f890746b0a7083efa9174716f3f14e09a53
guilhermesants <48955667+guilhermesants@users.noreply.github.com>
2019-04-11 16:12:08 -0300

    Update Jogo.java

--- Jogo.java
+++ Jogo.java
@@ -1,34 +1,21 @@
-
 package batalhanaval;
-
 
 public class Jogo {
     
-    int[][] tabelaJogo;
-    
+    String[][] tabelaJogo;
+ 
     public Jogo(){
-        this.tabelaJogo = new int[10][10];
+        this.tabelaJogo = new String[10][10];
     }
     
     
     
-    public void mostarTabela(){
+    public void mostrarTabela(){
         for (int i = 0; i < this.tabelaJogo.length; i++) {
             for (int j = 0; j < this.tabelaJogo[i].length; j++) {
                 System.out.print(this.tabelaJogo[i][j] + " ");
             }
             System.out.println("");
         }
-        
-        
-        
-    }
-
-    public int[][] getTabelaJogo() {
-        return tabelaJogo;
-    }
-
-    public void setTabelaJogo(int[][] tabelaJogo) {
-        this.tabelaJogo = tabelaJogo;
     }
 }
